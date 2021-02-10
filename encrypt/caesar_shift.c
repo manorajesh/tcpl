ba2050dcda2837eb29962b3dd915868aa02db250
Mano Rajesh <mano@raje.sh>
2021-02-09 20:05:07 -0800

    Iterated caesar_shift.c

--- encrypt/caesar_shift.c
+++ encrypt/caesar_shift.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+#include <ctype.h>
+
+#define MAXLINE 1000
+#define INCREMENT 1
+
+void caesarshift(int *in);
 
 int main()
 {
-	int c;
+	int c = 3;
+	char output[MAXLINE];
 
-	while ((c = getchar()) != EOF) {
-		printf ()
+	//fgets(output, MAXLINE, stdin);
+
+	caesarshift(&c);
+}
+
+void caesarshift(int *in)
+{
+	if (!isalpha(*in)) {
+		if (*in / 10) {
+			caesarshift(*in / 10);
+			*in += INCREMENT;
+		}
+	}
+	else if (isalpha(*in)) {
+		toupper(*in);
+
+		*in -= '0';
+		*in = (*in + INCREMENT) % 26;
+		*in += '0';
 	}
 }
